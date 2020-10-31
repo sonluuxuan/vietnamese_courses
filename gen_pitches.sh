e37467bcf17de3c4e6a38ab5024d4f69be86b0cc
son <luuxuansond@gmail.com>
2020-11-01 04:31:08 +0900

    fix gen_pitch file

--- gen_pitches.sh
+++ gen_pitches.sh
@@ -2,7 +2,7 @@
 
 COURSE_PATH=$1
 
-for i in ${COURSE_PATH}/*/*.mp3
+for i in ${COURSE_PATH}/*/*.wav
 do 
     # ffmpeg -i $i -ar 44100 ${i%.*}.wav
     ./pitch_detector -v -i ${i%.*}.wav -o ${i%.*}.pitch 
