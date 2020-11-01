2d113f63e8f932fbc8f5dbab28bce38ba664fc6d
son <luuxuansond@gmail.com>
2020-11-02 04:35:57 +0900

    add prof-courses' courses change wav files to mp3

--- gen_pitches.sh
+++ gen_pitches.sh
@@ -4,9 +4,9 @@ COURSE_PATH=$1
 
 for i in ${COURSE_PATH}/*/*.wav
 do 
-    # ffmpeg -i $i -ar 44100 ${i%.*}.wav
+    ffmpeg -i $i -ar 44100 ${i%.*}.wav
     ./pitch_detector -v -i ${i%.*}.wav -o ${i%.*}.pitch 
 done
 
-# rm ${COURSE_PATH}/*/*.wav
+rm ${COURSE_PATH}/*/*.wav
 
