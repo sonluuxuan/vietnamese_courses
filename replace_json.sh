a8fbbdbe2548bb6b6cc0dbeeb502cc8ab0bb0f50
son <luuxuansond@gmail.com>
2020-11-01 22:07:01 +0900

    finding python executable

--- replace_json.sh
+++ replace_json.sh
@@ -1,4 +1,5 @@
-/usr/bin/python modify_json.py info.json
+# /usr/bin/python modify_json.py info.json
 
-rm -f info.json
-mv info2.json info.json
+# rm -f info.json
+# mv info2.json info.json
+which python
